1739af7edbedfe07bf5b55e42e1cec9600cac0a3
Millana <chotubaevam@gmail.com>
2022-06-09 11:18:07 +0600

    want to implement with obj

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { ContextComponent } from "../context/CurrencyContext";
 import { Icon } from "react-icons-kit";
@@ -8,92 +8,141 @@ import { arrowSwap } from "react-icons-kit/ionicons/arrowSwap";
 
 export default function MainPageCharts() {
   const { allKeys } = useContext(ContextComponent);
+  
+  let obj = [
+    { first: "USD", second: "EUR" },
+    { first: "GBP", second: "EUR" },
+    { first: "USD", second: "JPY" },
+    { first: "GBP", second: "USD" },
+    { first: "USD", second: "KGS" },
+    { first: "USD", second: "KZT" },
+  ];
 
-  const [select1, setSelect1] = useState();
-  const [select2, setSelect2] = useState();
+  // console.log(obj);
+
+  // let differenceUsdEur;
+  // function checkUsdEur() {
+  //   let UsdEurValue = (
+  //     (allKeys["USD"]?.Value / allKeys["EUR"]?.Value) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   let UsdEurPrevious = (
+  //     (allKeys["USD"]?.Previous / allKeys["EUR"]?.Previous) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceUsdEur = (UsdEurPrevious - UsdEurValue).toFixed(2);
+  // }
+  // checkUsdEur();
+
+  // let differenceGbpEur;
+  // function checkGbpEur() {
+  //   let GbpEurValue = (
+  //     (allKeys["GBP"]?.Value / allKeys["EUR"]?.Value) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   let GbpEurPrevious = (
+  //     (allKeys["GBP"]?.Previous / allKeys["EUR"]?.Previous) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceGbpEur = (GbpEurPrevious - GbpEurValue).toFixed(2);
+  // }
+  // checkGbpEur();
+
+  // let differenceUsdJpy;
+  // function checkUsdJpy() {
+  //   let UsdJpyValue = (
+  //     (allKeys["USD"]?.Value / allKeys["JPY"]?.Value) *
+  //     allKeys["JPY"]?.Nominal
+  //   ).toFixed(2);
+  //   let UsdJpyPrevious = (
+  //     (allKeys["USD"]?.Previous / allKeys["JPY"]?.Previous) *
+  //     allKeys["JPY"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceUsdJpy = (UsdJpyPrevious - UsdJpyValue).toFixed(2);
+  // }
+  // checkUsdJpy();
+
+  // let differenceGbpUsd;
+  // function checkGbpUsd() {
+  //   let GbpUsdValue = (
+  //     (allKeys["GBP"]?.Value / allKeys["USD"]?.Value) *
+  //     allKeys["USD"]?.Nominal
+  //   ).toFixed(2);
+  //   let GbpUsdPrevious = (
+  //     (allKeys["GBP"]?.Previous / allKeys["USD"]?.Previous) *
+  //     allKeys["USD"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceGbpUsd = (GbpUsdPrevious - GbpUsdValue).toFixed(2);
+  // }
+  // checkGbpUsd();
+
+  // let differenceUsdKgs;
+  // function checkUsdKgs() {
+  //   let UsdKgsValue = (
+  //     (allKeys["USD"]?.Value / allKeys["KGS"]?.Value) *
+  //     allKeys["KGS"]?.Nominal
+  //   ).toFixed(2);
+  //   let UsdKgsPrevious = (
+  //     (allKeys["USD"]?.Previous / allKeys["KGS"]?.Previous) *
+  //     allKeys["KGS"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceUsdKgs = (UsdKgsPrevious - UsdKgsValue).toFixed(2);
+  // }
+  // checkUsdKgs();
+
+  // let differenceUsdKzt;
+  // function checkUsdKzt() {
+  //   let UsdKztValue = (
+  //     (allKeys["USD"]?.Value / allKeys["KZT"]?.Value) *
+  //     allKeys["KZT"]?.Nominal
+  //   ).toFixed(2);
+  //   let UsdKztPrevious = (
+  //     (allKeys["USD"]?.Previous / allKeys["KZT"]?.Previous) *
+  //     allKeys["KZT"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceUsdKzt = (UsdKztPrevious - UsdKztValue).toFixed(2);
+  // }
+  // checkUsdKzt();
+
   let differenceUsdEur;
-  function checkUsdEur() {
-    let UsdEurValue = (
-      (allKeys["USD"]?.Value / allKeys["EUR"]?.Value) *
-      allKeys["EUR"]?.Nominal
-    ).toFixed(2);
-    let UsdEurPrevious = (
-      (allKeys["USD"]?.Previous / allKeys["EUR"]?.Previous) *
-      allKeys["EUR"]?.Nominal
-    ).toFixed(2);
-    differenceUsdEur = (UsdEurPrevious - UsdEurValue).toFixed(2);
-  }
-  checkUsdEur();
+  // function checkUsdEur() {
+  //   let UsdEurValue = (
+  //     (allKeys["USD"]?.Value / allKeys["EUR"]?.Value) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   let UsdEurPrevious = (
+  //     (allKeys["USD"]?.Previous / allKeys["EUR"]?.Previous) *
+  //     allKeys["EUR"]?.Nominal
+  //   ).toFixed(2);
+  //   differenceUsdEur = (UsdEurPrevious - UsdEurValue).toFixed(2);
+  // }
+  // checkUsdEur();
 
-  let differenceGbpEur;
-  function checkGbpEur() {
-    let GbpEurValue = (
-      (allKeys["GBP"]?.Value / allKeys["EUR"]?.Value) *
-      allKeys["EUR"]?.Nominal
+  function checkBetween(el) {
+    let value = (
+      (allKeys[el.first]?.Value / allKeys[el.second]?.Value) *
+      allKeys[el.second]?.Nominal
     ).toFixed(2);
-    let GbpEurPrevious = (
-      (allKeys["GBP"]?.Previous / allKeys["EUR"]?.Previous) *
-      allKeys["EUR"]?.Nominal
+    let prev = (
+      (allKeys[el.first]?.Previous / allKeys[el.second]?.Previous) *
+      allKeys[el.second]?.Nominal
     ).toFixed(2);
-    differenceGbpEur = (GbpEurPrevious - GbpEurValue).toFixed(2);
+    differenceUsdEur = (prev - value).toFixed(2);
+    return differenceUsdEur;
   }
-  checkGbpEur();
+  const handleClick = (index) => {
+    let firstObj = obj[index]?.first;
+    // obj = obj.map((el) => [
+    //   { first: el[index]?.second, second: { ...firstObj } },
+    // ]);
+    obj[index] = { first: obj[index]?.second, second: firstObj };
+    console.log(obj);
+    return obj;
+  };
 
-  let differenceUsdJpy;
-  function checkUsdJpy() {
-    let UsdJpyValue = (
-      (allKeys["USD"]?.Value / allKeys["JPY"]?.Value) *
-      allKeys["JPY"]?.Nominal
-    ).toFixed(2);
-    let UsdJpyPrevious = (
-      (allKeys["USD"]?.Previous / allKeys["JPY"]?.Previous) *
-      allKeys["JPY"]?.Nominal
-    ).toFixed(2);
-    differenceUsdJpy = (UsdJpyPrevious - UsdJpyValue).toFixed(2);
-  }
-  checkUsdJpy();
-
-  let differenceGbpUsd;
-  function checkGbpUsd() {
-    let GbpUsdValue = (
-      (allKeys["GBP"]?.Value / allKeys["USD"]?.Value) *
-      allKeys["USD"]?.Nominal
-    ).toFixed(2);
-    let GbpUsdPrevious = (
-      (allKeys["GBP"]?.Previous / allKeys["USD"]?.Previous) *
-      allKeys["USD"]?.Nominal
-    ).toFixed(2);
-    differenceGbpUsd = (GbpUsdPrevious - GbpUsdValue).toFixed(2);
-  }
-  checkGbpUsd();
-
-  let differenceUsdKgs;
-  function checkUsdKgs() {
-    let UsdKgsValue = (
-      (allKeys["USD"]?.Value / allKeys["KGS"]?.Value) *
-      allKeys["KGS"]?.Nominal
-    ).toFixed(2);
-    let UsdKgsPrevious = (
-      (allKeys["USD"]?.Previous / allKeys["KGS"]?.Previous) *
-      allKeys["KGS"]?.Nominal
-    ).toFixed(2);
-    differenceUsdKgs = (UsdKgsPrevious - UsdKgsValue).toFixed(2);
-  }
-  checkUsdKgs();
-
-  let differenceUsdKzt;
-  function checkUsdKzt() {
-    let UsdKztValue = (
-      (allKeys["USD"]?.Value / allKeys["KZT"]?.Value) *
-      allKeys["KZT"]?.Nominal
-    ).toFixed(2);
-    let UsdKztPrevious = (
-      (allKeys["USD"]?.Previous / allKeys["KZT"]?.Previous) *
-      allKeys["KZT"]?.Nominal
-    ).toFixed(2);
-    differenceUsdKzt = (UsdKztPrevious - UsdKztValue).toFixed(2);
-  }
-  checkUsdKzt();
+  useEffect(() => {
+    console.log("mili");
+  }, [handleClick()]);
 
   return (
     <main className="chartPageMain">
@@ -111,168 +160,36 @@ export default function MainPageCharts() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1 USD</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 EUR</td>
-            <td>{differenceUsdEur}</td>
-            <td>
-              {differenceUsdEur >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>1 GBP</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 EUR</td>
-            <td>{differenceGbpEur}</td>
-            <td>
-              {differenceGbpEur >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>1 USD</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 JPY</td>
-            <td>{differenceUsdJpy}</td>
-            <td>
-              {differenceUsdJpy >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>1 GBP</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 USD</td>
-            <td>{differenceGbpUsd}</td>
-            <td>
-              {differenceGbpUsd >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>1 USD</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 KGS</td>
-            <td>{differenceUsdKgs}</td>
-            <td>
-              {differenceUsdKgs >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>1 USD</td>
-            <td style={{ border: "none" }}>
-              <div
-                style={{
-                  color: "#0a146d",
-                  marginLeft: "20px",
-                  textAlign: "center",
-                }}
-              >
-                <Icon size={30} icon={arrowSwap} />
-              </div>
-            </td>
-            <td>1 KZT</td>
-            <td>{differenceUsdKzt}</td>
-            <td>
-              {differenceUsdKzt >= 0 ? (
-                <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretUp} />
-                </h3>
-              ) : (
-                <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
-                  <Icon size={30} icon={caretDown} />
-                </h3>
-              )}
-            </td>
-          </tr>
+          {obj.map((el, index) => (
+            <tr>
+              <td>1 {el.first}</td>
+              <td style={{ border: "none" }}>
+                <div
+                  style={{
+                    color: "#0a146d",
+                    marginLeft: "20px",
+                    textAlign: "center",
+                  }}
+                  onClick={() => handleClick(index)}
+                >
+                  <Icon size={30} icon={arrowSwap} />
+                </div>
+              </td>
+              <td>1 {el.second}</td>
+              <td>{checkBetween(el)}</td>
+              <td>
+                {differenceUsdEur >= 0 ? (
+                  <h3 style={{ color: "#3f893c", marginLeft: "20px" }}>
+                    <Icon size={30} icon={caretUp} />
+                  </h3>
+                ) : (
+                  <h3 style={{ color: "#ea4729", marginLeft: "20px" }}>
+                    <Icon size={30} icon={caretDown} />
+                  </h3>
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
